480a3a05d417458b9be81f3f5557e10f36e210a3
jiaminqiang <jiaminqingx@gmail.com>
2018-04-15 19:48:00 +0800

    add ignore file

---
+++ Chapter-01-code/phonebook_func.py
@@ -0,0 +1,25 @@
+"""
+龙腾测试dev课程，通讯录程序
+实现一个简单的通讯录，包含增删改查
+"""
+record_list = []
+
+
+def input_record():
+    pass
+
+
+def add_record():
+    pass
+
+
+def query_record():
+    pass
+
+
+def delete_record():
+    pass
+
+
+def change_record():
+    pass
